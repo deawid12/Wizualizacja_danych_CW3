863229cc68d0d21308cb89169ffc6ea073db0eab
Dawid <dawid_plocharczyk3@o2.pl>
2021-03-14 17:48:23 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+import math
+
+
+# #Zad 1
+# for x in range(1,10):
+#     A = 1 - int(x)
+#
+# B = [4**i for i in range(0,7)]
+
+
+
+# #zad 4
+# a = input("Podaj przyprostokatna a : ")
+# b = input("Podaj przyprostokatna b: ")
+# c = input("Podaj przeciwprostokatna c: ")
+#
+# def czy_trojkat_prostokatny(a,b,c):
+#     if (int(a)+int(b))<=int(c):
+#         print("Trójkat nie powstanie")
+#     elif (int(a)**2+int(b)**2)==(int(c)**2):
+#         print("TROJKAT PROSTOKATNY")
+#     else:
+#         print("Trojkat nie jest prostokatny")
+# czy_trojkat_prostokatny(a,b,c)
+
+# #zad 5
+#
+# a = input("Podaj dlugosc gornej podstawy trapezu: ")
+# b = input("Podaj dlugosc dolnej podstawy trapezu: ")
+# c = input("Podaj wysokosc trapezu: ")
+# def pole_trapezu(a,b,c):
+#     pole=((int(a)+int(b))*int(c))/2
+#     print(pole)
+# pole_trapezu(a,b,c)
+
+#zad 6
+# def ciag(a=1,b=4,ile=10):
+#     jj=1
+#     mnożnik=1
+#     for x in range(ile):
+#         jj *= a*b
+#         mnożnik *=jj
+#         print(mnożnik)
+# print(ciag(a=1,b=4,ile=4))
+
